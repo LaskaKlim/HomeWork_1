a9600fd5c4306a94f5937a9fff14b972c9c1ae60
Laska <klimovskih.laska@mail.ru>
2022-12-04 04:12:11 +0700

    дз 3/4

--- HomeWork_4/Program.cs
+++ HomeWork_4/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+int M = 1;
+
+while (M <= N);
+{
+    if (M % 2 == 0)
+    {
+        Console.Write(M++);
+    }
+
+}
+  
