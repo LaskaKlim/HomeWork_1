d45f1ba4ad8a82100303fbcb4b466cf6a61c3f6c
Laska <klimovskih.laska@mail.ru>
2022-12-04 02:36:00 +0700

    Задача 1 рабочая

---
+++ First_s/Program.cs
@@ -0,0 +1,25 @@
+﻿// int number = 0;
+// string text = ""; //number = Convert.ToInt32(Console.ReadLine())
+// text = Console.ReadLine();
+// number = Convert.ToInt32(text);
+// Console.WriteLine("Result "  +  number * number);
+
+//Задача 1:
+
+//  Console.Write("Введите первое число ");
+
+// int numberA = Convert.ToInt32 (Console.ReadLine());
+ 
+//  Console.Write ("Введите второе число ");
+
+//  int numberB = Convert.ToInt32 (Console.ReadLine());
+
+//  if (numberA == numberB * numberB)
+//  {
+//     Console.Write("Да");
+//  }
+// else
+// {
+//   Console.Write ("Нет");  
+// }
+
